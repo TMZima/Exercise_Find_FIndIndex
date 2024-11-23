450b5adb60044bca20c282d8f0a7a225b4633948
Trey Zima <trey.m.zima@gmail.com>
2024-11-23 14:46:26 -0700

    Added comments to first task and completed second task

--- findFindIndex.js
+++ findFindIndex.js
@@ -8,8 +8,27 @@ const mythicalCreatures = [
 ];
 
 // Task 1: Use the 'find' method to locate the first creature of the 'Water' type and log its name to the console.
-mythicalCreatures.find((creature) => {
+
+// Use the 'find' method to locate the first creature of the 'Water' type.
+const firstWaterCreature = mythicalCreatures.find((creature) => {
+  // Check if the creature is of the 'Water' type.
   if (creature.type === "Water") {
+    // Log the name of the creature to the console.
     console.log(creature.name);
+    // Return true to indicate that this is the element we are looking for.
+    return true;
+  }
+});
+
+// Task 2: Use the 'findIndex' method to locate the index of the 'Griffin' in the mythical creatures array and log it to the console.
+
+// Use the 'findIndex' method to locate the index of the 'Griffin' in the mythical creatures array.
+const griffinIndex = mythicalCreatures.findIndex((creature) => {
+  // Check if the creature is the 'Griffin'.
+  if (creature.name === "Griffin") {
+    // Log the index of the 'Griffin' to the console.
+    console.log(mythicalCreatures.indexOf(creature));
+    // Return true to indicate that this is the element we are looking for.
+    return true;
   }
 });
